9e88b6b785376cb7d7b828cb6685ed310c1ac2a7
SKL Mindforge <shreemaniyer9@outlook.com>
2026-03-04 11:23:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skl_mindforge",
-    version="0.2.3", 
+    version="0.2.4", 
     packages=find_packages(),
     install_requires=["tokenizers>=0.13.0"],
     include_package_data=True,
